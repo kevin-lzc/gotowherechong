792d2292f9eb67945c08201a7ae851238041080f
ljylzc999867921 <2291014880@qq.com>
2020-08-17 15:54:35 +0800

    高德地图集成完成

---
+++ app/src/main/java/com/example/gotowherechong/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.gotowherechong.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gotowherechong.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
